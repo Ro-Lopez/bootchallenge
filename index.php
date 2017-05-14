7d6f11f15470c3fab95ac8f00e171fd74ed758e2
Robbie Lopez <rolopez.email@gmail.com>
2017-05-14 12:44:58 -0600

    put in header, 3 body, and footer and other stuff

--- index.php
+++ index.php
@@ -1,16 +1,8 @@
-
-
-
 <!DOCTYPE html>
-
 <html lang="en">
-
 	<head>
-
 		<meta charset="utf-8">
-
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 
 		<!-- Latest compiled and minified CSS -->
@@ -22,22 +14,41 @@
 		<!-- Font Awesome -->
 		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css" rel="stylesheet" />
 
-		<!--
+		<!-- MY Custom CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
 
+		<!-- jQuery -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" type="text/javascript"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 	</head>
 
 	<body>
+
+s
+
+
 
 		<header>
 
 			<div class="container">
 				<div class="row">
 
-					<div class="col-md-"></div>
-					<div class="col-md-"></div>
+					<div class="col-md-3">
+						<p>something</p>
+					</div>
+					<div class="col-md-3">
+						<p>something else</p>
+					</div>
 
 				</div>
 			</div>
@@ -47,7 +58,7 @@
 		<div class="container">
 			<div class="row">
 
-				<div clas="col-md-"></div>
+				<div class="col-md-"></div>
 
 			</div>
 		</div>
@@ -96,10 +107,4 @@
 		</div>
 
 	</footer>
-
-
-
-
-
-
 </html>
